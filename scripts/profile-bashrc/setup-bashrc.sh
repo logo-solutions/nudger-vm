69b6b71fd3a155b6103b38c4b0c9f5d29a8d63c0
logo <loic@logo-solutions.fr>
2025-10-09 18:48:22 +0000

    fix: init

--- scripts/profile-bashrc/setup-bashrc.sh
+++ scripts/profile-bashrc/setup-bashrc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 PROFILE_PATH="${PROFILE_PATH:-$HOME/nudger-vm/scripts/profile-bashrc/profile_logo.sh}"
 BLOCK_TAG="# >>> logo-bashrc >>>"
